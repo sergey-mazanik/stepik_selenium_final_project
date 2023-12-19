29b6019ddb78073e6d9a7eaac62c9a2d5c4f9dab
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2023-12-20 02:26:37 +0300

    modify some files

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 from .pages.locators import ProductPageLocators
 from time import sleep
 import pytest
@@ -16,7 +17,7 @@ import pytest
 #                                       marks=pytest.mark.xfail),
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
@@ -71,3 +72,5 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
